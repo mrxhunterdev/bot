a14a5e9b620bbb9bc273921c0f59de4af798824f
Mr.X-HUNTER <105683837+mrxhunterdev@users.noreply.github.com>
2023-09-17 01:42:32 +0700

    update app.py

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ api_id = 2427314;
 api_hash = "db7262e94f4881d8f813de0ae719f8c0";
 bot_token = "6489315176:AAHoqhkIHCbZkY2oFRDggeB1h-98INQ2Xrg";
 
-app = Client(
+def main():
+    with Client(
     "my_bot",
     api_id=api_id, api_hash=api_hash,
     bot_token=bot_token
-)
-app.send_message("me", "Greetings from **Pyrogram**!")
+) as app:
+     app.send_message("me", "Greetings from **Pyrogram**!")
 
-app.run()
+main()
