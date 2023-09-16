ab3305b918dc42fd18f91859715a304f42c9b850
Mr.X-HUNTER <105683837+mrxhunterdev@users.noreply.github.com>
2023-09-17 01:50:14 +0700

    update app.py

--- app.py
+++ app.py
@@ -4,16 +4,8 @@ api_id = 2427314;
 api_hash = "db7262e94f4881d8f813de0ae719f8c0";
 bot_token = "6489315176:AAHoqhkIHCbZkY2oFRDggeB1h-98INQ2Xrg";
 
-app = Client(
-    "my_bot",
-    api_id=api_id, api_hash=api_hash,
-    bot_token=bot_token
-)
+def main():
+    with Client("my_account", api_id, api_hash, bot_token) as app:
+     app.send_message("me", "Greetings from **Pyrogram**!")
 
-
-#@app.on_message(filters.private)
-#async def hello(client, message):
- #   await message.reply("Hello from Pyrogram!")
-###
-
-app.run()
+main()
