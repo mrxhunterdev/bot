246443d1ed103d623f914b64506dbe8ff7b7ff1a
Mr.X-HUNTER <105683837+mrxhunterdev@users.noreply.github.com>
2023-09-17 01:15:01 +0700

    app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+
+api_id = 2427314;
+api_hash = "db7262e94f4881d8f813de0ae719f8c0";
+bot_token = "6489315176:AAHoqhkIHCbZkY2oFRDggeB1h-98INQ2Xrg";
+
+app = Client(
+    "my_bot",
+    api_id=api_id, api_hash=api_hash,
+    bot_token=bot_token
+)
+
+app.run()
